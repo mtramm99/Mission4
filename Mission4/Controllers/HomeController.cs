d16484b9e07101dedbb323eb041e82f9df27e59e
Mark Trammell <mtramm99@gmail.com>
2022-02-08 12:56:02 -0700

    Add project files.

---
+++ Mission4/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission4.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission4.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private MovieInfoContext blahContext { get; set; }
+        public HomeController(ILogger<HomeController> logger, MovieInfoContext someName)
+        {
+            _logger = logger;
+            blahContext = someName;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult MyPodcasts()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult MovieForm ()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult MovieForm(MovieResponse mr)
+        {
+            blahContext.Add(mr);
+            blahContext.SaveChanges();
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Mission4/Models/MovieInfoContext.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission4.Models
+{
+    public class MovieInfoContext : DbContext
+    {
+        //Constructor
+        public MovieInfoContext (DbContextOptions<MovieInfoContext> options) : base (options)
+        {
+            //Leave blank for now
+        }
+
+        public DbSet<MovieResponse> Responses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<MovieResponse>().HasData(
+
+                new MovieResponse
+                {
+                    MovieId = 1,
+                    Category = "Action/Adventure",
+                    Title = "Kick-@$$",
+                    Year = 2010,
+                    Director = "Matthew Vaughn",
+                    Rating = "R",
+                    Edited = true,
+                },
+                new MovieResponse
+                {
+                    MovieId = 2,
+                    Category = "Family",
+                    Title = "Ratatouille",
+                    Year = 2007,
+                    Director = "Brad Bird, Jan Pinkava",
+                    Rating = "G",
+                    Edited = false,
+                }, 
+                new MovieResponse
+                {
+                    MovieId = 3,
+                    Category = "Family",
+                    Title = "Saturday's Warrior",
+                    Year = 1989,
+                    Director = "Bob Williams",
+                    Rating = "G",
+                    Edited = false,
+                }
+                );
+        }
+    }
+}
